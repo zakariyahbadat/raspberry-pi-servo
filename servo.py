523b84e4a908fe12f69b049902c983c48547bcbc
zakariyahbadat <zakariyahbadat@gmail.com>
2026-07-11 19:03:43 +0100

    attempt to prevent servo jitter

--- servo.py
+++ servo.py
@@ -8,14 +8,26 @@ pwm.start(0)
 
 try:
     while True:
-    
-        pwm.ChangeDutyCycle(3)
+        # Move to 0 degrees
+        pwm.ChangeDutyCycle(2.5)
+        time.sleep(0.5)            # Give it half a second to physically arrive
+        pwm.ChangeDutyCycle(0)     # TURN OFF THE PULSE (Stops jittering!)
+        time.sleep(2)              # Sit silently for 2 seconds
+        
+        # Move to 90 degrees
+        pwm.ChangeDutyCycle(7.5)
+        time.sleep(0.5)            # Give it time to move
+        pwm.ChangeDutyCycle(0)     # TURN OFF THE PULSE
         time.sleep(2)
-        pwm.ChangeDutyCycle(6.5)
-        time.sleep(2)
+        
+        # Move to 180 degrees (using 11.5 to prevent straining)
         pwm.ChangeDutyCycle(11.5)
+        time.sleep(0.5)            # Give it time to move
+        pwm.ChangeDutyCycle(0)     # TURN OFF THE PULSE
         time.sleep(2)
-        pwm.ChangeDutyCycle(0)
+
+
+
 except KeyboardInterrupt:
     print("Program stopped by user")
 
